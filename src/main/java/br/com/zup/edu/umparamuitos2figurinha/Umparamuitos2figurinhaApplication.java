c4bbd721247b35eafff7fd6f8496cf0c5ba8bff9
Denes <jose.araujo@zup.com.br>
2022-04-06 15:29:06 -0300

    criando a aplicacao

---
+++ src/main/java/br/com/zup/edu/umparamuitos2figurinha/Umparamuitos2figurinhaApplication.java
@@ -0,0 +1,13 @@
+package br.com.zup.edu.umparamuitos2figurinha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Umparamuitos2figurinhaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Umparamuitos2figurinhaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/zup/edu/umparamuitos2figurinha/Umparamuitos2figurinhaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zup.edu.umparamuitos2figurinha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Umparamuitos2figurinhaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
